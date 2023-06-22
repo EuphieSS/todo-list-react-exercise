3657de2a2bd9448ca45fe4d69935873db4bc440b
--replace-all <samma118@hotmail.com>
2023-06-21 22:06:56 -0400

    Add input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 
 function App() {
-  return <form className='new-item-form'>
-    <div className='form-row'>
-      <label htmlFor='item'>New Item</label>
-      <input type='text' id='item' />
-    </div>
-  </form>
+  return (
+    <form className='new-item-form'>
+      <div className='form-row'>
+        <label htmlFor='item'>New Item</label>
+        <input type='text' id='item' />
+      </div>
+      <button className='btn'>Add</button>
+    </form>
+  )
 }
 
 export default App;
